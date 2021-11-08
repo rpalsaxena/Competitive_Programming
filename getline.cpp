cb7a7d79d480c71ef633556aa4021b4f575bdfc5
Rahul Saxena <rpalsaxena@gmail.com>
2021-11-08 20:20:49 +0530

    Create getline.cpp

---
+++ getline.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+	int tests;
+	cin>> tests;
+	cin.ignore();
+	string str1, str2;
+	while( tests--){
+		getline(cin, str1);
+		cout<<str1<<"\n";	
+	}
+	
+}
